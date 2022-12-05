e5cfe2fbad49e96df6f8946faa5c6f9597d4e0fc
rivindum <rivindum.15@cse.mrt.ac.lk>
2022-12-05 11:38:11 +0530

    db connectivity

--- java/rest-api/src/main/java/wso2/example/byoc/service/ProductService.java
+++ java/rest-api/src/main/java/wso2/example/byoc/service/ProductService.java
@@ -5,7 +5,12 @@ import wso2.example.byoc.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -17,6 +22,41 @@ public class ProductService {
     }
 
     public List<Product> getProducts() {
+        Connection conn = null;
+
+        try
+        {
+            String getConsentResourcePrepStatement = "INSERT INTO testtable (ID) VALUES (?)";
+            conn = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/testchoreo","crootuser","crootuser");
+            System.out.println ("Database connection established");
+
+            try (PreparedStatement getConsentResourcePreparedStmt =
+                         conn.prepareStatement(getConsentResourcePrepStatement)) {
+                String uuid = String.valueOf(UUID.randomUUID());
+                getConsentResourcePreparedStmt.setString(1, uuid);
+                getConsentResourcePreparedStmt.execute();
+                System.out.println ("Database query executed : " + uuid);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close ();
+                    System.out.println ("Database connection terminated");
+                }
+                catch (Exception e) { /* ignore close errors */ }
+            }
+        }
         return repository.getAllProducts();
     }
 
@@ -31,5 +71,17 @@ public class ProductService {
 
     public Product updateProduct(Product product) {
        return repository.update(product);
+    }
+
+
+    private Connection getDBConnection() {
+
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://112.134.175.240:3306/choreo","root","root");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
